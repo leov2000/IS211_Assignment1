707c1a2b18a02d70672669f934dd66970e2d2471
Leonel Vergara <leovergarah@gmail.com>
2019-09-02 11:04:06 -0400

    (Added) Constructor level DocString for Book class

--- assignment1_part2.py
+++ assignment1_part2.py
@@ -7,6 +7,12 @@ class Book:
         title (str): The book title of an author.
     """
     def __init__(self, author, title):
+        """
+        The constructor for the Book class.
+        Parameters:
+            author (str): The name of an author.
+            title (str): The book title of an author. 
+        """
         self.author = author
         self.title = title
 
