c6bbac9659508dde33d5d274a702974387585208
Leonel Vergara <leovergarah@gmail.com>
2019-09-02 10:41:46 -0400

    (Added) comments to fn: listDivide, testDivide

--- assignment1_part1.py
+++ assignment1_part1.py
@@ -1,12 +1,15 @@
 def listDivide(numbers, divide = 2):
+    #If `numbers` has no items, return a 0.
     if not numbers:
         return 0
-    
+
+    #If `numbers` has items, extract `n`'s that are fully divisible by `divide`.      
     remainderList = [n for n in numbers if n % divide == 0]
 
+    # If the `remainderList` has items return the length of it.
     if remainderList:
         return len(remainderList)
-
+    #Else raise a custom error ListDivideException exception.
     else: 
         raise ListDivideException
 
@@ -16,7 +19,8 @@ def testDivide():
     listDivide([30, 54, 63,98, 100], divide=10)
     listDivide([])
     listDivide([1,2,3,4,5], 1)
+    # Comment-Out line below to test Exception logic
+    # listDivide([1], 5)
 
 class ListDivideException(Exception):
     pass
-
