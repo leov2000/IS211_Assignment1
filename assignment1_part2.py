37c296c5c36b138e498e81b5a6d480656f369c80
Leonel Vergara <leovergarah@gmail.com>
2019-09-02 11:03:41 -0400

    (Added) Top level DocString for Book class

--- assignment1_part2.py
+++ assignment1_part2.py
@@ -1,5 +1,11 @@
 class Book:
+    """
+    This is a class used to create Book objects.
 
+    Attributes:
+        author (str): The name of an author.
+        title (str): The book title of an author.
+    """
     def __init__(self, author, title):
         self.author = author
         self.title = title
