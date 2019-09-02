91af7d422c6b43f2af7ba2f7c015ffb4c74e2497
Leonel Vergara <leovergarah@gmail.com>
2019-09-02 10:44:53 -0400

    (Added) DocString for fn: listDivide

--- assignment1_part1.py
+++ assignment1_part1.py
@@ -1,4 +1,34 @@
 def listDivide(numbers, divide = 2):
+    """a function that takes a number list, a 
+    divide param and returns the amount of items that are 
+    divisible by the divide param.
+
+    Args:
+    numbers - List[Int]: A list of integer values.
+    divide - Int: A integer value.
+    
+    Returns | Raises:
+    An integer representing the sum of the list items divisible by divide
+    or an Exception if a scenario defined by the examples below fails.
+
+    Examples:
+    >>> listDivide([1,2,3,4,5])
+    2
+    >>> listDivide([2,4,6,8,10])
+    5
+    >>> listDivide([30, 54, 63,98, 100], divide=10)
+    2
+    >>> listDivide([])
+    0
+    >>> listDivide([1,2,3,4,5], 1)
+    5
+    >>> listDivide([1], 5)
+    Traceback (most recent call last):
+        File "assignment1_part1.py", line ##, in listDivide
+        raise ListDivideException
+        __main__.ListDivideException
+    """
+
     #If `numbers` has no items, return a 0.
     if not numbers:
         return 0
